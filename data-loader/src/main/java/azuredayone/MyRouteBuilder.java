fefbc030bab2e5bd3c883ab1f79bcd2473c92e78
lucidguppy <1146938+lucidguppy@users.noreply.github.com>
2023-12-08 16:23:02 -0500

    Initial commit

---
+++ data-loader/src/main/java/azuredayone/MyRouteBuilder.java
@@ -0,0 +1,16 @@
+package azuredayone;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class MyRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:foo?period={{myPeriod}}")
+            .bean("myBean", "hello")
+            .log("${body}")
+            .bean("myBean", "bye")
+            .log("${body}")
+            .log("THIS OTHER MESSAGE");
+    }
+}
